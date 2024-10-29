edac9455cc7ebfeb05a71d99e9eb29d2d5bb3f7c
Sasha Aldrick <sasha@aldrick.dev>
2024-10-29 14:00:37 +0000

    fixes to get it working

--- methods/build.rs
+++ methods/build.rs
@@ -1,35 +1,31 @@
-// Copyright 2023 RISC Zero, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 use std::{collections::HashMap, env, fs::File, io::Write, path::Path};
 
 use risc0_build::{embed_methods_with_options, DockerOptions, GuestOptions};
 use risc0_build_ethereum::generate_solidity_files;
 
-// Paths where the generated Solidity files will be written.
-const SOLIDITY_IMAGE_ID_PATH: &str = "../contracts/ImageID.sol";
-const SOLIDITY_ELF_PATH: &str = "../tests/Elf.sol";
-
 fn main() {
-    // Builds can be made deterministic, and thereby reproducible, by using Docker to build the
-    // guest. Check the RISC0_USE_DOCKER variable and use Docker to build the guest if set.
+    let current_dir = env::current_dir().unwrap();
+    println!("cargo:warning=Current directory: {:?}", current_dir);
+
+    // create directories if they don't exist
+    let contracts_dir = current_dir.join("../contracts");
+    let tests_dir = current_dir.join("../tests");
+    
+    std::fs::create_dir_all(&contracts_dir).unwrap();
+    std::fs::create_dir_all(&tests_dir).unwrap();
+
+    // use absolute paths
+    let image_id_path = contracts_dir.join("ImageID.sol");
+    let elf_path = tests_dir.join("Elf.sol");
+
+    println!("cargo:warning=ImageID.sol path: {:?}", image_id_path);
+    println!("cargo:warning=Elf.sol path: {:?}", elf_path);
+
     println!("cargo:rerun-if-env-changed=RISC0_USE_DOCKER");
     let use_docker = env::var("RISC0_USE_DOCKER").ok().map(|_| DockerOptions {
         root_dir: Some("../".into()),
     });
 
-    // Generate Rust source files for the methods crate.
     let guests = embed_methods_with_options(HashMap::from([(
         "guests",
         GuestOptions {
@@ -38,10 +34,13 @@ fn main() {
         },
     )]));
 
-    // Generate Solidity source files for use with Forge.
     let solidity_opts = risc0_build_ethereum::Options::default()
-        .with_image_id_sol_path(SOLIDITY_IMAGE_ID_PATH)
-        .with_elf_sol_path(SOLIDITY_ELF_PATH);
+        .with_image_id_sol_path(image_id_path.to_str().unwrap())
+        .with_elf_sol_path(elf_path.to_str().unwrap());
+
+    // try creating empty files first to verify permissions
+    File::create(&image_id_path).unwrap();
+    File::create(&elf_path).unwrap();
 
     generate_solidity_files(guests.as_slice(), &solidity_opts).unwrap();
 }
